47d9014bef4c0f4dc93a6dd807693e61621376e1
lucien06 <luochun3731@sina.com>
2017-07-21 16:30:55 +0800

    add load_test_case and its test cases

--- api_test/utils.py
+++ api_test/utils.py
@@ -1,9 +1,12 @@
 import hashlib
 import json
+import os
 import random
 import string
 
 import yaml
+
+from api_test.exception import ParamsError
 
 
 def gen_random_string(str_len):
@@ -23,6 +26,23 @@ def load_yaml_file(yaml_file):
     with open(yaml_file) as file:
         return yaml.load(file)
 
+
+def load_test_cases(test_cases_path):
+    file_suffix = os.path.splitext(test_cases_path)[1]
+    if file_suffix == '.json':
+        return load_json_file(test_cases_path)
+    elif file_suffix == '.yaml':
+        return load_yaml_file(test_cases_path)
+    else:
+        raise ParamsError('incorrect test case file name!')
+
+
+def parse_response(resp):
+    return {
+        'status_code': resp.status_code,
+        'headers': resp.headers,
+        'content': resp.content
+    }
 
 if __name__ == '__main__':
     # print(gen_random_string(8))
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,10 +1,12 @@
 import os
-import unittest
 
-from api_test import utils
+import requests
+
+from api_test import exception, utils
+from test.test_base import TestBase
 
 
-class TestUtils(unittest.TestCase):
+class TestUtils(TestBase):
     def test_load_json_file(self):
         file_name = os.path.join(os.getcwd(), 'test/data/demo.json')
         cases = utils.load_json_file(file_name)
@@ -18,6 +20,31 @@ class TestUtils(unittest.TestCase):
     def test_load_yaml_file(self):
         file_name = os.path.join(os.getcwd(), 'test/data/demo.yaml')
         cases = utils.load_yaml_file(file_name)
+        self.assertEqual(len(cases), 2)
+        self.assertEqual('create user which does not exist', cases[0]['test']['name'])
+        self.assertIn('headers', cases[0]['test']['request'])
+        self.assertIn('url', cases[0]['test']['request'])
+        self.assertIn('method', cases[0]['test']['request'])
+        self.assertEqual('create user which does exist', cases[1]['test']['name'])
+
+    def test_load_cases_with_bad_path(self):
+        test_case_path = os.path.join(os.getcwd(), 'test/data/demo')
+        with self.assertRaises(exception.ParamsError):
+            utils.load_test_cases(test_case_path)
+
+    def test_load_json_cases(self):
+        test_case_path = os.path.join(os.getcwd(), 'test/data/demo.json')
+        cases = utils.load_test_cases(test_case_path)
+        self.assertEqual(len(cases), 2)
+        self.assertEqual('create user which does not exist', cases[0]['test']['name'])
+        self.assertIn('headers', cases[0]['test']['request'])
+        self.assertIn('url', cases[0]['test']['request'])
+        self.assertIn('method', cases[0]['test']['request'])
+        self.assertEqual('create user which existed', cases[1]['test']['name'])
+
+    def test_load_yaml_cases(self):
+        test_case_path = os.path.join(os.getcwd(), 'test/data/demo.yaml')
+        cases = utils.load_test_cases(test_case_path)
         self.assertEqual(len(cases), 2)
         self.assertEqual('create user which does not exist', cases[0]['test']['name'])
         self.assertIn('headers', cases[0]['test']['request'])
@@ -39,3 +66,11 @@ class TestUtils(unittest.TestCase):
         md5 = utils.gen_md5('param1', 'param2')
         self.assertEqual(32, len(md5))
         self.assertIsInstance(md5, str)
+
+    def test_parse_response(self):
+        url = "http://127.0.0.1:5000/api/users"
+        resp = requests.get(url)
+        parse_result = utils.parse_response(resp)
+        self.assertIn('status_code', parse_result)
+        self.assertIn('headers', parse_result)
+        self.assertIn('content', parse_result)
